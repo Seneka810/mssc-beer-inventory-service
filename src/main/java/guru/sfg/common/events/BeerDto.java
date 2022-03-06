abe38ffb3df14f4b6ebf3b5c89ca7ffd3394d23e
seneka810 <kordyk.stanislav@gmail.com>
2022-03-06 09:06:41 +0200

    added jms logic

---
+++ src/main/java/guru/sfg/common/events/BeerDto.java
@@ -0,0 +1,57 @@
+package guru.sfg.common.events;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    static final long serialVersionUID = 7475389821576059083L;
+
+    @Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String beerName;
+
+    @NotNull
+    private String beerStyle;
+
+    @NotNull
+    private String upc;
+
+    @Positive
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal price;
+    @Positive
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,20 @@
+package guru.sfg.common.events;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -5465587622001599752L;
+
+    private BeerDto beerDto;
+
+}
+
